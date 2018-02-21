cbdd236e0c5594faf3ac09d0bd3d34a50d3348eb
Daniel Frey <dmfrey@gmail.com>
2018-02-21 17:47:48 -0500

    updated test

--- command/src/test/java/io/pivotal/dmfrey/eventStoreDemo/domain/client/kafka/KafkaBoardClientEmbeddedKafkaTests.java
+++ command/src/test/java/io/pivotal/dmfrey/eventStoreDemo/domain/client/kafka/KafkaBoardClientEmbeddedKafkaTests.java
@@ -1,34 +1,26 @@
 package io.pivotal.dmfrey.eventStoreDemo.domain.client.kafka;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.pivotal.dmfrey.eventStoreDemo.Application;
 import io.pivotal.dmfrey.eventStoreDemo.domain.client.BoardClient;
-import io.pivotal.dmfrey.eventStoreDemo.domain.client.kafka.config.KafkaClientConfig;
-import io.pivotal.dmfrey.eventStoreDemo.domain.client.kafka.config.KafkaConfig;
-import io.pivotal.dmfrey.eventStoreDemo.domain.client.kafka.service.DomainEventSink;
-import io.pivotal.dmfrey.eventStoreDemo.domain.client.kafka.service.DomainEventSource;
-import io.pivotal.dmfrey.eventStoreDemo.domain.client.kafka.service.DomainEventSourceImpl;
 import io.pivotal.dmfrey.eventStoreDemo.domain.events.BoardInitialized;
 import io.pivotal.dmfrey.eventStoreDemo.domain.events.DomainEvents;
 import io.pivotal.dmfrey.eventStoreDemo.domain.model.Board;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.Before;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.kafka.config.KafkaListenerEndpointRegistry;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.core.ProducerFactory;
-import org.springframework.kafka.listener.MessageListenerContainer;
 import org.springframework.kafka.test.rule.KafkaEmbedded;
-import org.springframework.kafka.test.utils.ContainerTestUtils;
 import org.springframework.kafka.test.utils.KafkaTestUtils;
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import java.time.Instant;
 import java.util.Map;
@@ -37,79 +29,87 @@ import java.util.UUID;
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertThat;
 
-@RunWith( SpringRunner.class )
-@SpringBootTest( classes = { KafkaConfig.class, KafkaClientConfig.class, DomainEventSink.class },
-        webEnvironment= SpringBootTest.WebEnvironment.NONE,
-        properties = {
-            "spring.cloud.service-registry.auto-registration.enabled=false" //,
-//            "spring.cloud.stream.bindings.input.consumer.useNativeDecoding=true",
-//            "spring.cloud.stream.bindings.output.producer.useNativeEncoding=true",
-//            "spring.cloud.stream.bindings.input.group=group",
-//            "spring.cloud.stream.kafka.streams.binder.serdeError=sendToDlq",
-//            "spring.cloud.stream.kafka.streams.binder.configuration.default.value.serde=" +
-//                    "org.apache.kafka.common.serialization.Serdes$IntegerSerde"
-        }
-)
-@ActiveProfiles( "kafka" )
-@DirtiesContext
 @Slf4j
 public class KafkaBoardClientEmbeddedKafkaTests {
 
     private static String RECEIVER_TOPIC = "board-events";
 
-    private KafkaTemplate<String, String> template;
-
-    @Autowired
-    private KafkaListenerEndpointRegistry kafkaListenerEndpointRegistry;
-
     @ClassRule
     public static KafkaEmbedded embeddedKafka = new KafkaEmbedded( 1, true, RECEIVER_TOPIC);
 
-    @Before
-    public void setUp() throws Exception {
+    private static Consumer<String, String> consumer;
 
-        // set up the Kafka producer properties
-        Map<String, Object> senderProperties = KafkaTestUtils.senderProps( embeddedKafka.getBrokersAsString() );
+    @BeforeClass
+    public static void setUp() throws Exception {
 
-        // create a Kafka producer factory
-        ProducerFactory<String, String> producerFactory = new DefaultKafkaProducerFactory<>( senderProperties );
+        Map<String, Object> consumerProps = KafkaTestUtils.consumerProps("command-board-events-group", "false", embeddedKafka );
+        consumerProps.put( ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest" );
 
-        // create a Kafka template
-        template = new KafkaTemplate<>( producerFactory );
-        // set the default topic to send to
-        template.setDefaultTopic( RECEIVER_TOPIC );
+        DefaultKafkaConsumerFactory<String, String> cf = new DefaultKafkaConsumerFactory<>( consumerProps );
 
-        // wait until the partitions are assigned
-        for( MessageListenerContainer messageListenerContainer : kafkaListenerEndpointRegistry.getListenerContainers() ) {
+        consumer = cf.createConsumer();
 
-            ContainerTestUtils.waitForAssignment( messageListenerContainer, embeddedKafka.getPartitionsPerTopic() );
+        embeddedKafka.consumeFromAnEmbeddedTopic( consumer, RECEIVER_TOPIC );
+
+    }
+
+    @AfterClass
+    public static void tearDown() {
+
+        consumer.close();
+
+    }
+
+    @Test
+    public void testFind() throws Exception {
+
+        SpringApplication app = new SpringApplication( Application.class );
+        app.setWebApplicationType( WebApplicationType.NONE );
+        ConfigurableApplicationContext context = app.run("--server.port=0",
+                "spring.cloud.service-registry.auto-registration.enabled=false",
+                "--spring.jmx.enabled=false",
+                "--spring.cloud.stream.bindings.input.destination=board-events",
+                "--spring.cloud.stream.bindings.output.destination=board-events",
+                "--spring.cloud.stream.kafka.streams.binder.configuration.commit.interval.ms=1000",
+//                "--spring.cloud.stream.kafka.streams.binder.configuration.default.key.serde=org.apache.kafka.common.serialization.Serdes$StringSerde",
+//                "--spring.cloud.stream.kafka.streams.binder.configuration.default.value.serde=org.apache.kafka.common.serialization.Serdes$StringSerde",
+                "--spring.cloud.stream.bindings.output.producer.headerMode=raw",
+                "--spring.cloud.stream.bindings.input.consumer.headerMode=raw",
+                "--spring.cloud.stream.kafka.streams.binder.brokers=" + embeddedKafka.getBrokersAsString(),
+                "--spring.cloud.stream.kafka.streams.binder.zkNodes=" + embeddedKafka.getZookeeperConnectionString(),
+                "--spring.profiles.active=kafka");
+        try {
+
+            receiveAndValidateBoard( context );
+
+        } finally {
+
+            context.close();
 
         }
 
     }
 
-    @Autowired
-    BoardClient boardClient;
+    private void receiveAndValidateBoard( ConfigurableApplicationContext context ) throws Exception {
 
-    @Autowired
-    ObjectMapper mapper;
+        Map<String, Object> senderProps = KafkaTestUtils.producerProps( embeddedKafka );
+        DefaultKafkaProducerFactory<Integer, String> pf = new DefaultKafkaProducerFactory<>( senderProps );
+        KafkaTemplate<Integer, String> template = new KafkaTemplate<>( pf, true );
+        template.setDefaultTopic( RECEIVER_TOPIC );
 
-    @MockBean
-    DomainEventSource domainEventSource;
-
-    @Test
-    public void testFind() throws Exception {
+        ObjectMapper mapper = context.getBean( ObjectMapper.class );
+        BoardClient boardClient = context.getBean( BoardClient.class );
 
         UUID boardUuid = UUID.randomUUID();
         BoardInitialized boardInitialized = createTestBoardInitializedEvent( boardUuid );
         String event = mapper.writeValueAsString( boardInitialized );
-        this.template.sendDefault( event );
+        template.sendDefault( event );
 
         DomainEvents domainEvents = new DomainEvents();
         domainEvents.setBoardUuid( boardUuid );
         domainEvents.getDomainEvents().add( createTestBoardInitializedEvent( boardUuid ) );
 
-        Board board = this.boardClient.find( boardUuid );
+        Board board = boardClient.find( boardUuid );
         assertThat( board, is( notNullValue() ) );
         assertThat( board.getBoardUuid(), is( equalTo( boardUuid ) ) );
         assertThat( board.getName(), is( equalTo( "New Board" ) ) );
